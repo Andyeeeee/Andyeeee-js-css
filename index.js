15c871b8a497db3088cb20c9a35440da25c708c9
Andyeeeee <10514f074@gmail.com>
2024-02-29 13:52:58 +0800

    E

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { scrollHandler } from "./js/Andyeeee.js";
+console.log(scrollHandler);
 // import { navbar } from '../compoment/aaaa.js'
 window.addEventListener('scroll', scrollHandler);
 /*這裡或是指定html的script擇一打 */
