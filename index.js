b5128add4585f6122fed1393b1e88220564b7fad
Andyeeeee <10514f074@gmail.com>
2024-02-29 13:48:28 +0800

    UPDTATE路近

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { scrollHandler } from "../js/Andyeeee.js";
+import { scrollHandler } from "./js/Andyeeee.js";
 // import { navbar } from '../compoment/aaaa.js'
 window.addEventListener('scroll', scrollHandler);
 /*這裡或是指定html的script擇一打 */
